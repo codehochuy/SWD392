8626f0afe01d425fe22c4b830c66afe9e63cd5ee
codehochuy <121989761+codehochuy@users.noreply.github.com>
2024-02-27 17:30:16 +0700

    cấu hình swagger

--- src/main/java/com/example/swd392/config/SecurityConfig.java
+++ src/main/java/com/example/swd392/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/auth/**",
                                 "/v2/api-docs",
                                 "/v3/api-docs",
-                                "/v3/api-docs/**",
+//                                "/v3/api-docs/**",
                                 "/swagger-resources",
                                 "/swagger-resources/**",
                                 "/configuration/ui",
