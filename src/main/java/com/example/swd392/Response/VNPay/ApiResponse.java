4cc44602b8ea1bb14f45ece99e2de2dd221290ba
codehochuy <121989761+codehochuy@users.noreply.github.com>
2024-02-29 22:59:28 +0700

    VNpay

---
+++ src/main/java/com/example/swd392/Response/VNPay/ApiResponse.java
@@ -0,0 +1,10 @@
+package com.example.swd392.Response.VNPay;
+
+import lombok.Data;
+
+@Data
+public class ApiResponse {
+    private String code;
+    private String message;
+    private String data;
+}
