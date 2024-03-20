59909ce18d92c515eaa074c528f6d9b5bd2bff81
codehochuy <121989761+codehochuy@users.noreply.github.com>
2024-03-21 03:28:45 +0700

    update account

--- src/main/java/com/example/swd392/Swd392Application.java
+++ src/main/java/com/example/swd392/Swd392Application.java
@@ -58,10 +58,10 @@ public class Swd392Application {
             AuthenticationService service
     ) {
         return args -> {
+            //supper admin
             var super_admin = RegisterRequest.builder()
                     .name("Super Admin")
                     .email("Superadmin@gmail.com")
-//					.avatar("admin.jpg")
                     .password("123")
                     .phone("0326514968")
                     .status(true)
@@ -69,11 +69,10 @@ public class Swd392Application {
                     .role(SUPER_ADMIN)
                     .build();
             System.out.println("Super_Admin token :" + service.register(super_admin).getAccessToken());
-
+            //admin
             var admin = RegisterRequest.builder()
                     .name("Admin")
                     .email("Admin@gmail.com")
-//					.avatar("admin.jpg")
                     .password("123")
                     .phone("0392272536")
                     .status(true)
@@ -82,28 +81,52 @@ public class Swd392Application {
                     .build();
             System.out.println("Admin token :" + service.register(admin).getAccessToken());
 
+            //audience
             var audience = RegisterRequest.builder()
-                    .name("David12")
-                    .email("david@gmail.com")
-//					.avatar("huy.jpg")
+                    .name("Audience1")
+                    .email("huypt110402@gmail.com")
                     .status(true)
                     .password("123")
                     .phone("0854512367")
+                    .account_balance(0.0)
                     .role(AUDIENCE)
                     .build();
-            audience.setAccount_balance(1000000.0);
             System.out.println("Audience token :" + service.register(audience).getAccessToken());
+
+            var audience2 = RegisterRequest.builder()
+                    .name("Audience2")
+                    .email("tuanpn123012@gmail.com")
+                    .status(true)
+                    .password("123")
+                    .phone("0854512367")
+                    .account_balance(0.0)
+                    .role(AUDIENCE)
+                    .build();
+            service.register(audience2);
+
+            //creator
+
             var creator = RegisterRequest.builder()
                     .name("Huy")
                     .email("huypt160548@fpt.edu.vn")
-//    .avatar("huy.jpg")
                     .status(true)
                     .password("123")
                     .phone("0326514875")
                     .role(CREATOR)
+                    .account_balance(0.0)
                     .build();
-            creator.setAccount_balance(1000000.0);
             System.out.println("Creator token :" + service.register(creator).getAccessToken());
+
+            var creator2 = RegisterRequest.builder()
+                    .name("Vuong")
+                    .email("votrongvuong001@gmail.com")
+                    .status(true)
+                    .password("123")
+                    .phone("0326514875")
+                    .role(CREATOR)
+                    .account_balance(0.0)
+                    .build();
+            service.register(creator2);
 
         };
     }
--- src/main/java/com/example/swd392/config/CorsConfig.java
+++ src/main/java/com/example/swd392/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // Thay đổi origin này cho phù hợp với origin của ứng dụng frontend của bạn
+                .allowedOrigins("http://localhost:5174") // Thay đổi origin này cho phù hợp với origin của ứng dụng frontend của bạn
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
